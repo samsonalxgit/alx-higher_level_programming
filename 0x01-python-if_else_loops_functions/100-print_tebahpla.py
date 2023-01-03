245f252b9e03057095e96830741725822a0018a0
samsonalxgit <samsonmanie2022@gmail.com>
2023-01-03 12:50:20 -0800

    tebah

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 for c in range(ord('z'), ord('a') - 1, -1):
-    print("{}".format(chr(c)) if c % 2 == 0
-          else "{}".format(chr(c).upper()), end="")
+    print("{}".format(chr(c)) 
+            if c % 2 == 0
+            else "{}".format(chr(c).upper()), end="")
