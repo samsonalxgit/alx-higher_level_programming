9ff985236eb548407348601691d63b4bfef3ca1a
samsonalxgit <samsonmanie2022@gmail.com>
2023-01-03 12:47:08 -0800

    tebah

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
-# Author -Samson Manie
-
-i = 0
 for c in range(ord('z'), ord('a') - 1, -1):
-    print("{}".format(chr(c - i)), end="")
-    i = 32 if i == 0 else 0
+    print("{}".format(chr(c)) if c % 2 == 0
+          else "{}".format(chr(c).upper()), end="")
