80262606689e8ed8c10389a4b08d461b881ddf98
samsonalxgit <samsonmanie2022@gmail.com>
2023-01-09 03:06:19 -0800

    one element

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+
+def element_at(my_list, idx):
+    """
+    Gets an element in a list at a given index
+    And returns it
+    """
+    list_len = len(my_list)
+    if idx >= list_len or idx < 0:
+        return
+    return (my_list[idx])
