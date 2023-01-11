4d6533d7dd7e63dbdc910039c508969e036e3d38
samsonalxgit <samsonmanie2022@gmail.com>
2023-01-11 13:29:47 -0800

    number of keys in a dictionary

---
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+
+def number_keys(a_dictionary):
+    """
+    counts the number of keys in a dictionary and returns it
+    """
+    return(len(a_dictionary))
+Footer
+© 2023 GitHub, Inc.
+Footer navigation
+Terms
+Priv
