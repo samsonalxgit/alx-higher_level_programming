490f78a8c28b9276f598d61f601cdfa761e5bfd2
samsonalxgit <samsonmanie2022@gmail.com>
2023-04-10 13:58:19 -0700

    JavaScript Introduction

--- 0x12-javascript-warm_up/0-javascript_is_amazing.js
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -1,7 +1,5 @@
 #!/usr/bin/node
-//To print a string 'Javascript is amazing
+// To write A string 'JavaScript is amazing'
 
-const myVar;
-myVar = "JavaScript is amazing";
-
+const myVar = 'JavaScript is amazing';
 console.log(myVar);
