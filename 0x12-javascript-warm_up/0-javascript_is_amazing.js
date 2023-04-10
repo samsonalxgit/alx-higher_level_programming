7ecb105e4169b2061c7e57409c5097fcabd54cc7
samsonalxgit <samsonmanie2022@gmail.com>
2023-04-10 14:23:31 -0700

    multi language

--- 0x12-javascript-warm_up/0-javascript_is_amazing.js
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/node
-// To write A string 'JavaScript is amazing'
-
-const myVar = 'JavaScript is amazing';
-console.log(myVar);
---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,7 @@
+#!/usr/bin/nod
+//To write three languages of coding
+
+let myVar = 'C is fun';
+myVar = 'Python is cool';
+myVar = 'JavaScript';
+console.log(myVar);
