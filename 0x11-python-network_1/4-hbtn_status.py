bc8c67c7b0882c36573752d8f084ea541bd7dd77
samsonalxgit <samsonmanie2022@gmail.com>
2023-04-28 14:08:15 -0700

    url_fetching

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -6,7 +6,7 @@ Fetches https://intranet.hbtn.io/status using requests module
 import requests
 
 if __name__ == '__main__':
-    r = requests.get('https://intranet.hbtn.io/status')
+    r = requests.get('https://alx-intranet.hbtn.io/status')
     print('Body response:')
     print('\t- type: {}'.format(type(r.text)))
     print('\t- content: {}'.format(r.text))
