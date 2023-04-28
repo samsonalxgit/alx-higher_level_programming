c9bb03e58e203cb23e86e051dd8cfd1e6bf1f6a6
samsonalxgit <samsonmanie2022@gmail.com>
2023-04-28 13:31:18 -0700

    url_fetch

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
+#Python script that fetches https://alx-intranet.hbtn.io/status
 
+#You must use the package urllib
+#You are not allowed to import any packages other than urllib
+#The body of the response must be displayed like the following example (tabulation before -)
+#You must use a with statement
 import urllib.request
 
 with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
