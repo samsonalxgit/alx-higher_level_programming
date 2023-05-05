7205151cd0871f07b7b004e16f08c7385f91b4d6
samsonalxgit <samsonmanie2022@gmail.com>
2023-05-05 02:37:56 -0700

    Js_ is _ amazing

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,9 +1,6 @@
 #!/usr/bin/nod
 //To write three languages of coding
 
-const lang1 = 'C is fun';
-const lang2 = 'Python is cool';
-const lang3 = 'JavaScript';
-console.log(lang1);
-console.log(lang2);
-console.log(lang3);
+console.log('C is fun');
+console.log('Python is cool');
+console.log('JavaScript is amazing');
