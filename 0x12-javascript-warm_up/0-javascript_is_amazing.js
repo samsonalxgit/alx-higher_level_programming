0fb8f0a9a784c4cc865048ee8982fb779c82bcdf
samsonalxgit <samsonmanie2022@gmail.com>
2023-05-05 02:07:35 -0700

    JS_warmup

--- 0x12-javascript-warm_up/0-javascript_is_amazing.js
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -2,5 +2,5 @@
 #create a constant variable called myVar with the value “JavaScript is amazing”
 #must use console.log(...) to print all output
 #You are not allowed to use var
-
-console.log("JavaScript is amazing);
+const myvar = “JavaScript is amazing”;
+console.log("myvar);
