54e541770fde1d645ba74c317db16a5f1217dc2c
samsonalxgit <samsonmanie2022@gmail.com>
2023-05-05 02:45:04 -0700

    Three_languages

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,6 +1,4 @@
 #!/usr/bin/node
-//To write three languages of coding
-
 console.log('C is fun');
 console.log('Python is cool');
-console.log('JavaScript is amazing');
+console.log('Javascript is amazing');
