1bd1a94367bac3cf650b4764e634ce4e559d5ff6
samsonalxgit <samsonmanie2022@gmail.com>
2023-05-05 03:48:44 -0700

    Arguments

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -3,13 +3,11 @@
 
 const args = (count);
 
-if(count < 1){
-	console.log('No argument');
+if (count < 1){
+	console.log("No argument");
+} else if (count = 1){
+	console.log("Argument found");
+} else{
+	console.log("Arguments found");
 }
-else if(count = 1){
-	console.log('Argument found');
-}
-else{
-	console.log('Arguments found');
-}
-}
+
